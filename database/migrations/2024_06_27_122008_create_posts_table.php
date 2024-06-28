95a1708d786548ef2e98dd2f3ae13d9bd14d9b94
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-28 21:44:37 +0200

    Create Post migration file, factory and seed database.

---
+++ database/migrations/2024_06_27_122008_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Post;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('content');
+            $table->enum('type', Post::$types);
+            $table->boolean('archived')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
