003fe98a395bb89df082b6ec2f7ee3449da105e9
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-30 22:22:14 +0200

    Add basic navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,18 @@
 </head>
 
 <body class="bg-gray-400">
+    <nav>
+        <ul class="flex justify-between p-3">
+            <li>
+                <a href="{{ route('post.index') }}">Monaliza</a>
+            </li>
+            @foreach (['wierszem_pisane' => 'Wierszem Pisane', 'scenariusze_pisane_życiem' => 'Scenariusze Pisane Życiem', 'z_medycznego_punktu_widzenia' => 'Z Medycznego Punktu Widzenia', 'taniec' => 'Taniec'] as $key => $value)
+                <li>
+                    <a href="{{ route('post.index', ['type' => $key]) }}">{{ $value }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </nav>
     <div class="container mx-auto max-w-2xl px-2">
         {{ $slot }}
     </div>
