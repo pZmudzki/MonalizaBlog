92dc49052ea0cea8ce137024d74c3ae584b4a509
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-07-01 20:06:30 +0200

    Add remember input to login page

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -11,6 +11,10 @@
                 <label for="password">Hasło:</label>
                 <input type="password" name="password" id="password" class="rounded-md border border-black">
             </div>
+            <div>
+                <input type="checkbox" name="remember" id="remember" class="rounded-md border border-black">
+                <label for="remember">Zapamiętaj</label>
+            </div>
             <button type="submit"
                 class="bg-gray-200 rounded-md border border-black hover:bg-gray-300 transition">Zaloguj!</button>
         </form>
