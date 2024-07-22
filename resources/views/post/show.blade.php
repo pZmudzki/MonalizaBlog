4f8f67f5e12b4d91083db1e93b8aeb689d509ac6
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-07-22 19:34:02 +0200

    Show videos and images in post show view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,5 +1,17 @@
 <x-layout>
     <x-post-card :post="$post">
-        <div>{{ $post->content }}</div>
+        <div class="grid grid-cols-2">
+            <div>{{ $post->content }}</div>
+            @foreach ($post->images as $image)
+                <img src="{{ url('/') }}/storage/{{ $image->filepath }}" alt="{{ $image->filename }}">
+            @endforeach
+        </div>
+        <div class="flex justify-center mt-4">
+            @foreach ($post->videos as $video)
+                <video controls>
+                    <source src="{{ url('/') }}/storage/{{ $video->filepath }}" type="video/mp4">
+                </video>
+            @endforeach
+        </div>
     </x-post-card>
 </x-layout>
