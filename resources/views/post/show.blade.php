ecf17f35bc94d382d906b9fc3bedb7f3692e0a7a
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-07-22 20:42:01 +0200

    Fix showing files in post show view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,15 +2,19 @@
     <x-post-card :post="$post">
         <div class="grid grid-cols-2">
             <div>{{ $post->content }}</div>
-            @foreach ($post->images as $image)
-                <img src="{{ url('/') }}/storage/{{ $image->filepath }}" alt="{{ $image->filename }}">
+            @foreach ($post->files as $file)
+                @if ($file->type === 'image')
+                    <img src="{{ url('/') }}/storage/{{ $file->filepath }}" alt="{{ $file->filename }}">
+                @endif
             @endforeach
         </div>
-        <div class="flex justify-center mt-4">
-            @foreach ($post->videos as $video)
-                <video controls>
-                    <source src="{{ url('/') }}/storage/{{ $video->filepath }}" type="video/mp4">
-                </video>
+        <div class="flex flex-col items-center mt-4 gap-2">
+            @foreach ($post->files as $file)
+                @if ($file->type === 'video')
+                    <video controls>
+                        <source src="{{ url('/') }}/storage/{{ $file->filepath }}">
+                    </video>
+                @endif
             @endforeach
         </div>
     </x-post-card>
