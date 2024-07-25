162ba4d29a9c2f319b73eefa7befbd2f9f8faffc
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-07-25 16:17:39 +0200

    Create aliases for middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\RegisterPostView;
+use App\Http\Middleware\RegisterUserVisit;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'visit' => RegisterUserVisit::class,
+            'view' => RegisterPostView::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
