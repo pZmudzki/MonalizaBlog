f6a3ad7dadbb89cdc1f9099069f03dd9b350f075
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-07-25 14:45:55 +0200

    Create relation methods in View and Visit models

---
+++ app/Models/View.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class View extends Model
+{
+    use HasFactory;
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+    public function visit(): BelongsTo
+    {
+        return $this->belongsTo(Visit::class);
+    }
+}
--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Visit extends Model
 {
     use HasFactory;
 
     protected $fillable = ['ip_address'];
+
+    public function views(): HasMany
+    {
+        return $this->hasMany(View::class);
+    }
 }
