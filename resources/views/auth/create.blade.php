438fa738eda56b847eb88b0c0b959fcfa46085fd
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-07-27 00:25:40 +0200

    Reuse input and label component in login view

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -4,16 +4,16 @@
             @csrf
             <h1 class="text-center text-2xl">Zaloguj się</h1>
             <div>
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" class="rounded-md border border-black">
+                <x-label for="email">Email:</x-label>
+                <x-text-input name="email" placeholder="Email" />
             </div>
             <div>
-                <label for="password">Hasło:</label>
-                <input type="password" name="password" id="password" class="rounded-md border border-black">
+                <x-label for="password">Hasło:</x-label>
+                <x-text-input name="password" placeholder="Hasło" />
             </div>
-            <div>
-                <input type="checkbox" name="remember" id="remember" class="rounded-md border border-black">
-                <label for="remember">Zapamiętaj</label>
+            <div class="flex gap-2">
+                <x-label for="remember">Zapamiętaj</x-label>
+                <input type="checkbox" name="remember" id="remember" class="mb-2 rounded-md border border-black">
             </div>
             <button type="submit"
                 class="bg-gray-200 rounded-md border border-black hover:bg-gray-300 transition">Zaloguj!</button>
