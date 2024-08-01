d524bda5e5d35be3c0e1d610fa5a4b35f3f00bb2
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-08-01 23:10:16 +0200

    Display success and error messages in layout under a navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,7 +26,22 @@
         'z_medycznego_punktu_widzenia' => 'Z Medycznego Punktu Widzenia',
     ]" />
 
+
     <div class="mx-auto mt-20">
+        @if (session('success'))
+            <div role="alert" class="border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
+                <p class="font-bold">Sukces!</p>
+                <p>{{ session('success') }}</p>
+            </div>
+        @endif
+
+        @if (session('error'))
+            <div role="alert" class="my-4 border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+                <p class="font-bold">Błąd!</p>
+                <p>{{ session('error') }}</p>
+            </div>
+        @endif
+
         {{ $slot }}
     </div>
 </body>
