2562763dbc402b9b53164daf6827058e25ff68b2
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-08-05 20:59:53 +0200

    Show post category over a title

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,4 +1,7 @@
 <x-card>
+    <h3 class="text-xs italic">
+        {{ ['wierszem_pisane' => 'Wierszem Pisane', 'scenariusze_pisane_życiem' => 'Scenariusze Pisane Życiem', 'z_medycznego_punktu_widzenia' => 'Z Medycznego Punktu Widzenia', 'taniec' => 'Taniec'][$post->type] }}
+    </h3>
     <div class="flex items-center justify-between mb-2">
         <h2 class="font-bold first-letter:uppercase truncate">{{ $post->title }}</h2>
         <span>{{ $post->created_at->diffForHumans() }}</span>
